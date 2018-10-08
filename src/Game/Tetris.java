f5ad8a89033f3e316a57467ab2e59c079717dcfc
Karan Saini <karan.saj@gmail.com>
2018-10-08 23:47:13 +0530

    Create class Tetris

--- src/Game/Tetris.java
+++ src/Game/Tetris.java
@@ -7,14 +7,33 @@ package Game;
  * @author Karan
  *
  */
+
+import java.util.*;
+import java.util.Scanner;
+
 public class Tetris {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		int r = 30, c = 30;
+		Scanner sc = new Scanner(System.in);
 
+		Board m = new Board(r, c);
+		m.assign();
+		m.display();
+
+		Shape s = new Shape(r, c);
+
+		while(true)
+		{
+			m.assign(s);
+			m.display();
+			String ch = sc.next();
+			s.execute(ch);
+		}
 	}
 
 }
