49f60b73aa26d7d052d960b1ee9b497684e6de76
Karan Saini <karan.saj@gmail.com>
2018-10-08 23:50:52 +0530

    Create class Point

---
+++ src/Game/Point.java
@@ -0,0 +1,22 @@
+package Game;
+
+class Point {
+	
+	private int x, y;
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
